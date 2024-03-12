cd5c4ab3662600daf3786ea9624433c41edfca09
Benceszalaiii <szalai.bence.bendeguz@students.jedlik.eu>
2024-03-12 23:00:16 +0100

    whatever, fine for now there are responsive issues, will need that fixed, + navbar

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
+
 function toggleDarkmode(){  // Handle toggle darkmode button
     if (document.body.classList.contains("dark"))
         document.body.classList.remove("dark")
     else
         document.body.classList.add("dark")
 }
-
-
